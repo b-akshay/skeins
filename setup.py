2e06ca9e2c4c40b7c9a36c62985383e50fea5c8f
b-akshay <akshay@akshay.bio>
2023-09-04 11:28:48 -0700

    cleanup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='skeins',
-	version='0.0.1',
+	version='0.1.0',
 	author='Akshay Balsubramani',
 	author_email='akshay@akshay.bio',
 	packages=['skeins'],
--- src/skeins/__init__.py
+++ src/skeins/__init__.py
@@ -5,6 +5,8 @@ from . import graph_prediction
 from . import graph_construction
 from . import graph_frequencies
 from . import graph_sparsification
+from . import graph_utils
+from . import visualization
 #from . import hierarchy_tools
 
 __version__ = "0.1.0"
